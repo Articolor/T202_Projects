009c4d2067c345b91b2497de835bf0b9226b5abd
86155 <2905933587@qq.com>
2019-11-21 19:45:32 +0800

    yuan321

--- wuliu_yslh/src/main/java/com/wl/model/SY_Emp.java
+++ wuliu_yslh/src/main/java/com/wl/model/SY_Emp.java
@@ -21,6 +21,51 @@ public class SY_Emp {
 
     private BigDecimal disabled;
 
+
+
+
+    private Integer page=1;//起始页数
+
+    private Integer rows=5;//到哪一行结束
+
+    //一个用户对应一个角色
+    private SY_Role sy_role;
+
+    //一个用户对应一个单位
+    private SY_Units sy_units;
+
+    public Integer getPage() {
+        return page;
+    }
+
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    public Integer getRows() {
+        return rows;
+    }
+
+    public void setRows(Integer rows) {
+        this.rows = rows;
+    }
+
+    public SY_Role getSy_role() {
+        return sy_role;
+    }
+
+    public void setSy_role(SY_Role sy_role) {
+        this.sy_role = sy_role;
+    }
+
+    public SY_Units getSy_units() {
+        return sy_units;
+    }
+
+    public void setSy_units(SY_Units sy_units) {
+        this.sy_units = sy_units;
+    }
+
     public BigDecimal getId() {
         return id;
     }
