02cfd7934b1c46152f0082217db3860ff7eb63ef
Administrator <2905933587@qq.com>
2019-11-21 19:24:44 +0800

    hu21

--- wuliu_yslh/src/main/java/com/wl/mapper/BAS_BasicarchivesMapper.java
+++ wuliu_yslh/src/main/java/com/wl/mapper/BAS_BasicarchivesMapper.java
@@ -1,9 +1,19 @@
 package com.wl.mapper;
 
 import com.wl.model.BAS_Basicarchives;
-import java.math.BigDecimal;
+import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+@Service
 public interface BAS_BasicarchivesMapper {
+    //查询基础档案
+    List<BAS_Basicarchives> findBAS_BasicarchivesAndSY_EmpAndSY_Units(int page,int rows);
+
+    //分页查询基础数据
+    List<BAS_Basicarchives> findfindBASBasicarchivesAndSYEmpAndSYUnitsKey(Integer parentid, String ename, String empname, Date boperationtime   ,  int page, int rows);
+
     int deleteByPrimaryKey(BigDecimal id);
 
     int insert(BAS_Basicarchives record);
