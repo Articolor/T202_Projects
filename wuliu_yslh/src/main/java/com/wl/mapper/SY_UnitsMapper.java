866bfeb7e07b00a3e6f5809b8cee874575d87b9b
86155 <2905933587@qq.com>
2019-11-22 09:08:51 +0800

    yuan322

--- wuliu_yslh/src/main/java/com/wl/mapper/SY_UnitsMapper.java
+++ wuliu_yslh/src/main/java/com/wl/mapper/SY_UnitsMapper.java
@@ -12,9 +12,14 @@ public interface SY_UnitsMapper {
     //查询单位和员工
     List<SY_Units> findAllUnitsAndEmp(SY_Units units);
 
-    //查询所 有的单位
+    //查询所有的单位
     List<SY_Units> findAllUnits();
 
+    //查询总行数
+    public Integer findCount();
+
+    //模糊查询后的总行数
+    public Integer findMhCount(SY_Units units);
 
     int deleteByPrimaryKey(BigDecimal id);
 
