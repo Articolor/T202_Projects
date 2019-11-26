073946178854e6d19076ad2ae3744482ccefc2ed
86155 <2905933587@qq.com>
2019-11-26 18:10:21 +0800

    yuan26-2

--- wuliu_yslh/src/main/java/com/wl/model/DIS_Propagandatask.java
+++ wuliu_yslh/src/main/java/com/wl/model/DIS_Propagandatask.java
@@ -16,6 +16,25 @@ public class DIS_Propagandatask {
 
     private String content;
 
+    private Integer page=1;//起始页数
+    private Integer rows=5;//到那一页
+
+    public Integer getPage() {
+        return page;
+    }
+
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    public Integer getRows() {
+        return rows;
+    }
+
+    public void setRows(Integer rows) {
+        this.rows = rows;
+    }
+
     public BigDecimal getId() {
         return id;
     }
