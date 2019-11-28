9f979c0fe0c4998131c9c65162590bc382abbbc3
Administrator <2905933587@qq.com>
2019-11-28 19:28:56 +0800

    hu28-10

--- wuliu_yslh/src/main/java/com/wl/service/BAS_ZoneinfoService.java
+++ wuliu_yslh/src/main/java/com/wl/service/BAS_ZoneinfoService.java
@@ -7,5 +7,7 @@ import java.util.List;
 public interface BAS_ZoneinfoService {
     //管理定区
     List<BAS_Zoneinfo> findBAS_ZoneinfoAndSY_EmpAndSY_Units(Integer page, Integer rows);
+    //管理定区查询
+    List<BAS_Zoneinfo> findBAS_ZoneinfoAndSY_EmpAndSY_Unitsfenye(String name,String zonecode,Integer page,Integer rows);
 
 }
