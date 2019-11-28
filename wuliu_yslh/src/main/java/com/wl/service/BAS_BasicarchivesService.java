75c8e9fc6826de95b11bda13876a8756e49d0d94
Administrator <2905933587@qq.com>
2019-11-28 19:28:37 +0800

    hu28-9

--- wuliu_yslh/src/main/java/com/wl/service/BAS_BasicarchivesService.java
+++ wuliu_yslh/src/main/java/com/wl/service/BAS_BasicarchivesService.java
@@ -10,4 +10,8 @@ public interface BAS_BasicarchivesService {
     List<BAS_Basicarchives> findBAS_BasicarchivesAndSY_EmpAndSY_Units(Integer page, Integer rows);
     //分页查询基础数据
     List<BAS_Basicarchives> findfindBASBasicarchivesAndSYEmpAndSYUnitsKey( String ename,  int page, int rows);
+    //新增数据
+    int insert(BAS_Basicarchives record);
+    //修改基础数据
+    int updateByPrimaryKey(BAS_Basicarchives record);
 }
--- wuliu_yslh/src/main/java/com/wl/service/BAS_PartitionService.java
+++ wuliu_yslh/src/main/java/com/wl/service/BAS_PartitionService.java
@@ -8,6 +8,6 @@ public interface BAS_PartitionService {
     //管理分区
     List<BAS_Partition> findBAS_Partition(Integer page, Integer rows);
     //高级查询管理分区
-    List<BAS_Partition> findBAS_PartitionGAOJI(String province, String city, String county, String zonecode, String keyword, Integer page, Integer rows);
+    List<BAS_Partition> findBAS_PartitionGAOJI(String province,String city,String county,Integer page, Integer rows);
 
 }
