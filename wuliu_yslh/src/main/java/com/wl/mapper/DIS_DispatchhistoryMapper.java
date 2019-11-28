aff9da8a69053ab49f691634a5016b01004c6edd
86155 <2905933587@qq.com>
2019-11-28 19:34:23 +0800

    yuan28-2

--- wuliu_yslh/src/main/java/com/wl/mapper/DIS_DispatchhistoryMapper.java
+++ wuliu_yslh/src/main/java/com/wl/mapper/DIS_DispatchhistoryMapper.java
@@ -1,12 +1,18 @@
 package com.wl.mapper;
 
 import com.wl.model.DIS_Dispatchhistory;
-import java.math.BigDecimal;
+import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+@Service
 public interface DIS_DispatchhistoryMapper {
     int deleteByPrimaryKey(BigDecimal id);
 
-    int insert(DIS_Dispatchhistory record);
+    //新增一条调度历史数据
+    void  insert(DIS_Dispatchhistory record);
+
+    //根据工单表的id查询出对应的调度历史表
+    DIS_Dispatchhistory findByDisp(BigDecimal no);
 
     int insertSelective(DIS_Dispatchhistory record);
 
