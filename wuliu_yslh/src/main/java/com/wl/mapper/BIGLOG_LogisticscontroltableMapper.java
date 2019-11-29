37a8b601bafd73f76898246b4e08308c5e6c0871
qiao'zhi <2905933587@qq.com>
2019-11-29 17:49:05 +0800

    qiqi29-3

--- wuliu_yslh/src/main/java/com/wl/mapper/BIGLOG_LogisticscontroltableMapper.java
+++ wuliu_yslh/src/main/java/com/wl/mapper/BIGLOG_LogisticscontroltableMapper.java
@@ -1,18 +1,29 @@
 package com.wl.mapper;
 
 import com.wl.model.BIGLOG_Logisticscontroltable;
+import com.wl.model.SY_Emp;
+import org.apache.ibatis.annotations.One;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Service;
+
 import java.math.BigDecimal;
-
+import java.util.List;
+@Service
 public interface BIGLOG_LogisticscontroltableMapper {
-    int deleteByPrimaryKey(BigDecimal id);
-
-    int insert(BIGLOG_Logisticscontroltable record);
+    public List<BIGLOG_Logisticscontroltable> selectAll(BIGLOG_Logisticscontroltable b);
+    public BIGLOG_Logisticscontroltable selectById(int id);
+    int delete(BIGLOG_Logisticscontroltable b);
+    public int querycount(Object o);
+    public int insert(BIGLOG_Logisticscontroltable record);
 
     int insertSelective(BIGLOG_Logisticscontroltable record);
 
-    BIGLOG_Logisticscontroltable selectByPrimaryKey(BigDecimal id);
+    public BIGLOG_Logisticscontroltable selectByPrimaryKey(BigDecimal id);
 
-    int updateByPrimaryKeySelective(BIGLOG_Logisticscontroltable record);
+    int update(BIGLOG_Logisticscontroltable record);
 
-    int updateByPrimaryKey(BIGLOG_Logisticscontroltable record);
+    public int updateByPrimaryKey(BIGLOG_Logisticscontroltable record);
+
 }
