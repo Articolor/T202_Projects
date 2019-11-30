8bf6bc83054a367619c897814bdeaf5e851f9835
qiao'zhi <2905933587@qq.com>
2019-11-30 21:38:13 +0800

    qiqi30-6

---
+++ wuliu_yslh/src/main/java/com/wl/controller/LQPAC_PackagingController.java
@@ -0,0 +1,33 @@
+package com.wl.controller;
+
+import com.wl.model.PAC_Packaging;
+import com.wl.service.PAC_PackagingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class LQPAC_PackagingController {
+    @Autowired
+    private PAC_PackagingService service;
+    @RequestMapping("querypack")
+    public Map<String,Object> querypack(PAC_Packaging p){
+        int page=p.getPage();
+        int rows=p.getRows();
+        p.setPage((page-1)*rows+1);
+        p.setRows(page*rows);
+        Map<String,Object> map=new HashMap<>();
+        List<PAC_Packaging> list=service.querypack(p);
+        int count =service.getcountlq(p);
+        map.put("list",list);
+        map.put("count",count);
+        for (PAC_Packaging pi : list) {
+            System.out.println(pi.getEmp().getEmpname());
+        }
+        return map;
+    }
+}
