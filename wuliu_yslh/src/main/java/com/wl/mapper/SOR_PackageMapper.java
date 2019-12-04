e07fae75dee915b28f16d812b90c9926d0d8dcc6
86155 <2905933587@qq.com>
2019-12-04 15:37:59 +0800

    yuan12-4-5

--- wuliu_yslh/src/main/java/com/wl/mapper/SOR_PackageMapper.java
+++ wuliu_yslh/src/main/java/com/wl/mapper/SOR_PackageMapper.java
@@ -9,13 +9,20 @@ import java.util.List;
 @Service
 public interface SOR_PackageMapper {
     int deleteByPrimaryKey(BigDecimal id);
+    //查询全部的合包信息
+    List<SOR_Package> findAllPackYWY(SOR_Package p);
 
+    //新增和包信息
     int insert(SOR_Package record);
+
+    //根据时间查询对象
+    List<SOR_Package> findNewDate();
 
     int insertSelective(SOR_Package record);
 
     SOR_Package selectByPrimaryKey(BigDecimal id);
 
+    //修改合包信息
     int updateByPrimaryKeySelective(SOR_Package record);
 
     int updateByPrimaryKey(SOR_Package record);
--- wuliu_yslh/src/main/java/com/wl/mapper/SOR_PackagedetailsMapper.java
+++ wuliu_yslh/src/main/java/com/wl/mapper/SOR_PackagedetailsMapper.java
@@ -1,11 +1,14 @@
 package com.wl.mapper;
 
 import com.wl.model.SOR_Packagedetails;
-import java.math.BigDecimal;
+import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+@Service
 public interface SOR_PackagedetailsMapper {
     int deleteByPrimaryKey(BigDecimal id);
 
+    //新增和包详细信息
     int insert(SOR_Packagedetails record);
 
     int insertSelective(SOR_Packagedetails record);
